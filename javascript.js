e683d72e0d9bf099a311ebff6ce695c19bd1d7df
alunoEtec <aula@etec.com>
2025-10-08 20:49:17 -0300

    começando com js

---
+++ javascript.js
@@ -0,0 +1,25 @@
+var nome = "Eliezer"
+var idade = "17"
+var idadePassada = 10
+var anoPassado = 2024
+var anoAtual = '2025'
+var peso = 78.5
+var altura = 1.70
+var luzNaoApagada = false
+var SemNamorada = true
+
+//var nome = prompt("Digite seu nome: ")
+//alert(nome)    //ou pode ser   alert("O nome digitado foi: " + nome)
+/*
+    Crie  3 variaveis,
+    peça para o usuario digitar 3 numeros
+    e exiba em um alerta a soma deles
+*/
+
+var bank1 = prompt("Digite um numero: ") 
+var bank2 = prompt("Digite outro numeor: ")
+var bank3 = prompt("Digite o ultimo numero: ")
+
+var soma = (bank1 + bank2 + bank3)
+
+alert("A somas dos bancos sao: " + soma)
